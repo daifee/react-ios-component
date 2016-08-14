fdcee5aa9274c6233142b731223f5eba202225bd
haw <794873321@qq.com>
2016-08-14 11:42:20 +0800

    modify dir

--- website/build.sh
+++ website/build.sh
@@ -4,14 +4,14 @@ reactPath='./node_modules/react/dist/*'
 reactDomPath='./node_modules/react-dom/dist/*'
 iscrollPath='./node_modules/iscroll/build/*'
 distPath='./dist/*'
-libPath='./website/lib'
+libPath='./website/example/lib'
 examplePath='./website/example'
 
 # 复制依赖包
-cp $reactPath $libPath
-cp $reactDomPath $libPath
-cp $iscrollPath $libPath
-cp $distPath $example
+cp -r $reactPath $libPath
+cp -r $reactDomPath $libPath
+cp -r $iscrollPath $libPath
+cp -r $distPath $examplePath
 
 # 打包组件 demo
 webpack --progress --colors --config website/webpack.config.website.js
